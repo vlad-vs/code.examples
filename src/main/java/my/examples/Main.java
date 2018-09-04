c8d2e29fe7280f615582025f6327adfe7dd57428
vladvs <vladislav.semenov@privatbank.ua>
2018-09-04 16:48:40 +0300

    add pettern fabricka

--- src/main/java/my/examples/Main.java
+++ src/main/java/my/examples/Main.java
@@ -11,6 +11,30 @@ public class Main {
         Coffe coffe = new Coffe();
 
         Coffe.CoffeSize.MEDIUM.getMll();
+
+
+        GeozoneExistsType isExists = GeozoneExistsType.IS_EXISTS;
+        System.out.println(isExists.b);
+
+        System.out.println(isExists.isExist());
+    }
+
+    public enum GeozoneExistsType {
+
+        IS_EXISTS (true, "The current zone is open to the user"),
+        NOT_EXIST (false, "The current zone is closed to the user");
+
+        private boolean b;
+        private String res;
+
+        GeozoneExistsType(boolean b, String res) {
+            this.b = b;
+            this.res = res;
+        }
+
+        public boolean isExist(){
+            return b;
+        }
     }
 
 
